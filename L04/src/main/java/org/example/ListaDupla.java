bd12dfc365e4ce7aade33ea73616538143534f8a
fb00s <153862225+fb00s@users.noreply.github.com>
2025-03-18 21:40:05 -0300

    Update ListaDupla.java

--- L04/src/main/java/org/example/ListaDupla.java
+++ L04/src/main/java/org/example/ListaDupla.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class ListaDupla<T> {
     private NoListaDupla<T> primeiro;
-    private NoListaDupla<T> proximo;
-    private NoListaDupla<T> anterior;
 
     public ListaDupla(){
         this.primeiro = null;
